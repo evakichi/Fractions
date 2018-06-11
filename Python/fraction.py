2481b458899fa954dfc402e89857c84b3489ed5f
evakichi <evakichi@be.wakwak.com>
2018-06-11 22:09:57 +0900

    sample code

--- Python/fraction.py
+++ Python/fraction.py
@@ -1,9 +1,29 @@
+import math
 class fraction:
         def __init__(self,nu,de):
-            self.nu=nu
-            self.de=de
+            self.nu=int(nu)
+            self.de=int(de)
+            if self.de == 0:
+                raise Exception('denominator is Zero')
             if self.de<0:
                 self.nu=self.nu*(-1)
                 self.de=self.de*(-1)
-        def print_frac():
-            print (str(self.nu)+"/"+str(self.de))
+            gcd=math.gcd(abs(self.nu),abs(self.de))
+            if gcd!=0:
+                self.nu=self.nu/gcd
+                self.de=self.de/gcd
+
+        def print_frac(self):
+            print (str(int(self.nu))+"/"+str(int(self.de)))
+
+        def add(self,frac0):
+            return fraction(self.nu*frac0.de+self.de*frac0.nu,self.de*frac0.de)
+
+        def sub(self,frac0):
+            return fraction(self.nu*frac0.de-self.de*frac0.nu,self.de*frac0.de)
+
+        def mul(self,frac0):
+            return fraction(self.nu*frac0.nu,self.de*frac0.de)
+
+        def div(self,frac0):
+            return fraction(self.nu*frac0.de,self.de*frac0.nu)
--- Python/test.py
+++ Python/test.py
@@ -1,4 +1,16 @@
 import fraction
 
-frac = fraction(1,1)
+frac = fraction.fraction(2,4)
 frac.print_frac()
+
+frac1 = frac.add(fraction.fraction(1,2))
+frac1.print_frac()
+
+frac1 = frac.sub(fraction.fraction(1,2))
+frac1.print_frac()
+
+frac1 = frac.mul(fraction.fraction(1,2))
+frac1.print_frac()
+
+frac1 = frac.div(fraction.fraction(1,2))
+frac1.print_frac()
